918b9f1318992d88f09f6ed0e15a5c84e38e7aa3
Your Name <you@example.com>
2024-05-09 10:14:28 -0300

    feat:ex3.js

--- ex3.js
+++ ex3.js
@@ -1,4 +1,9 @@
 // Exercício 3: Ímpar ou par
 // Faça um programa que receba um número do usuário e informe se este número é par ou ímpar.
 
-let 
+let numb = parseFloat(prompt("Digite um número"))
+
+if (numb % 2 == 0) {
+    console.log("o numero é par")}
+
+    else(console.log ("o numero é impar"))
